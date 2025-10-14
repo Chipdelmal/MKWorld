9c61e274cf386d8dcd0122e086a46fb504129b4b
chipdelmal <chipdelmal@gmail.com>
2025-10-13 19:25:37 -0700

    Dash plot improved

--- radar.py
+++ radar.py
@@ -2,6 +2,9 @@ import pickle as pkl
 from os import path
 from itertools import product
 import plotly.graph_objects as go
+from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
 import pandas as pd
 import constants as cst
 ###############################################################################
@@ -28,13 +31,13 @@ dfCmb = pd.DataFrame({
 dfCmb = dfCmb.join(dfCls)
 ixplit = [i.split('-') for i in dfCmb.index]
 dfCmb['Racer'] = [
-    i[0].strip().replace(' ', '') 
+    i[0].strip().replace('', '') 
     for i in ixplit
 ]
 dfCmb['Kart'] = [
-    i[1].strip().replace(' ', '') 
+    i[1].strip().replace('', '') 
     if len(i)==2 else
-    '-'.join(i[1:]).replace(' ', '')
+    '-'.join(i[1:]).replace('', '')
     for i in ixplit
 ]
 dfCmb.rename({
@@ -64,7 +67,7 @@ cats = [
 
 (cA, cB) = (
     'Yoshi - Standard Kart',
-    'Daisy - Standard Kart'
+    'Toadette - Cute Scoot'
 )
 
 pA = dfCmb.loc[cA][cats]+10
@@ -87,6 +90,73 @@ fig.update_layout(
             visible=False,
             range=[0, 25]
     )),
+    width=1000,
+    height=1000,
     showlegend=True
 )
 fig.show()
+
+
+
+
+charList = sorted(list(set(dfCmb['Racer'].values)))
+kartList = sorted(list(set(dfCmb['Kart'].values)))
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.layout = html.Div([
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(charList, 'Yoshi', id='charA'),
+            dcc.Dropdown(kartList, 'Standard Kart', id='kartA'),
+        ], width=2),
+        dbc.Col([
+            dcc.Dropdown(charList, 'Toadette', id='charB'),
+            dcc.Dropdown(kartList, 'Cute Scoot', id='kartB'),
+        ], width=2),
+    ]),
+    html.Div(
+        dcc.Graph(figure=fig, id='radar'), 
+        id='plot'
+    )
+])
+@callback(
+    Output('radar', 'figure'),
+    Input('charA', 'value'), Input('kartA', 'value'),
+    Input('charB', 'value'), Input('kartB', 'value'),
+)
+def update_figure(charA, kartA, charB, kartB):
+
+    (cA, cB) = (
+        f'{charA} - {kartA}',
+        f'{charB} - {kartB}'
+    )
+
+    pA = dfCmb.loc[cA][cats]+10
+    pB = dfCmb.loc[cB][cats]+10
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatterpolar(
+        r=pA, theta=cats,
+        # fill='toself',
+        name=cA
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=pB, theta=cats,
+        # fill='toself',
+        name=cB
+    ))
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=False,
+                range=[0, 25]
+        )),
+        width=1000,
+        height=1000,
+        showlegend=True
+    )
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8051)
